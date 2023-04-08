557dccb9c57e5deef496137b31947fb5cd0ba4e8
marisol varela <marisol.varela.cab@gmail.com>
2023-04-08 19:09:42 +0200

    change added

--- Scroll_animation/script.js
+++ Scroll_animation/script.js
@@ -7,8 +7,8 @@ checkBoxes();
 function checkBoxes () {
    const triggerBottom = window.innerHeight / 5 * 4;
   
-   const boxTop = box.getBoundingClientRect().top;
    boxes.forEach(box => {
+       const boxTop = box.getBoundingClientRect().top;
 
        if(boxTop < triggerBottom) {
            box.classList.add('show')
